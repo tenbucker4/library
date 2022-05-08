66e144d019a31aee29f07f4b3290cb4112c0d5fa
Ben Tucker <bwtucker4@gmail.com>
2022-05-08 14:19:05 -0700

    Base layout

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 let myLibrary = [];
 
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.getInfo = function() {
-        console.log(title);
-        console.log(author);
-        console.log(pages);
-        console.log(read);
-    }
 }
 
 const book1 = new book("Harry Potter", "JK Rowling", "789", "read");
 
 function addBookToLibrary() {
-    //
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
-console.log(book1.getInfo());
