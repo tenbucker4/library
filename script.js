09ac4a3de332d61e05b74bb434faa0671ffdbc46
Ben Tucker <bwtucker4@gmail.com>
2022-05-09 13:14:27 -0700

    Add book to library based on user input

--- script.js
+++ script.js
@@ -11,18 +11,22 @@ function Book(Title, Author, Pages, Read) {
 function addBookToLibrary(Title, Author, Pages, Read) {
     let book = new Book(Title, Author, Pages, Read);
     myLibrary.push(book);
+    displayBooks();
 }
 
 // Display library array
 function displayBooks() {
     const books = document.querySelector(".content");
+    const removeCards = document.querySelectorAll(".card");
+    for (let i = 0; i < removeCards.length; i++) {
+        removeCards[i].remove();
+    }
 
     myLibrary.forEach(myLibrary => {
         const card = document.createElement("div");
         card.classList.add("card");
         books.appendChild(card);
         for (let key in myLibrary) {
-            console.log(`${key}: ${myLibrary[key]}`);
             const para = document.createElement("p");
             para.textContent = (`${key}: ${myLibrary[key]}`);
             card.appendChild(para);
@@ -30,11 +34,21 @@ function displayBooks() {
     })
 }
 
-addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
-addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
-addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
-addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
-addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
-addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
+// Display form when "New Book" button is clicked
+function displayForm() {
+    document.querySelector(".book-form").style.display = "";
+}
 
-displayBooks();
+// Submit book to library when submit button is clicked
+const submitBook = document.querySelector(".submit-btn");
+
+function addFormData() {
+    let Title = document.getElementById("Title").value;
+    let Author = document.getElementById("Author").value;
+    let Pages = document.getElementById("Pages").value;
+    let Read = document.getElementById("Read").value;
+
+
+    addBookToLibrary(Title, Author, Pages, Read);
+    document.getElementById("add-book").reset();
+}
