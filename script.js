d1f33bdea9e68532f81a5c7b4b3127b693e18861
Ben Tucker <bwtucker4@gmail.com>
2022-05-09 11:09:01 -0700

    Manually add books to library and display on page using DOM manipulation

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// Book object constructor
+function Book(Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
 }
 
-const book1 = new book("Harry Potter", "JK Rowling", "789", "read");
-
-function addBookToLibrary() {
-    let book = new Book(title, author, pages, read);
+function addBookToLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
     myLibrary.push(book);
 }
 
+// Display library array
+function displayBooks() {
+    const books = document.querySelector(".content");
+
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            card.appendChild(para);
+        }
+    })
+}
+
+addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
+addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
+addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
+addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
+addBookToLibrary("Harry Potter", "JK Rowling", "650", "Read");
+addBookToLibrary("The Blood of Elves", "Andrej Sapkowsky", "650", "Read");
+
+displayBooks();
