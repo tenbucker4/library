6bcb4c5f76c80e09f6c9a8bc494a463334b95bf1
Ben Tucker <bwtucker4@gmail.com>
2022-05-09 20:57:03 -0700

    Start fresh. Code was an absolute mess

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
 // Book object constructor
-function Book(Title, Author, Pages, Read) {
-    this.Title = Title;
-    this.Author = Author;
-    this.Pages = Pages;
-    this.Read = Read;
+class Book {
+    constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    }
 }
 
 // Display form when "New Book" button is clicked
@@ -16,72 +18,3 @@ function displayForm() {
 // Submit book to library when submit button is clicked
 const form = document.getElementById("add-book");
 const submitBook = document.querySelector(".submit-btn");
-
-function addFormData() {
-    let Title = document.getElementById("Title").value;
-    let Author = document.getElementById("Author").value;
-    let Pages = document.getElementById("Pages").value;
-    let Read = document.getElementById("Read").value;
-
-    if ((Title == "") || (Author == "") || (Pages == "") || (Read == "")) {
-        return;
-    }
-
-    addBookToLibrary(Title, Author, Pages, Read);
-}
-
-function addBookToLibrary(Title, Author, Pages, Read) {
-    let book = new Book(Title, Author, Pages, Read);
-    myLibrary.push(book);
-    displayBooks();
-    form.reset();
-}
-
-// Clear Form
-const clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", clearForm());
-
-function clearForm() {
-    document.getElementById("add-book").reset();
-}
-
-// Display library array
-function displayBooks() {
-    const books = document.querySelector(".content");
-    const removeCards = document.querySelectorAll(".card");
-    for (let i = 0; i < removeCards.length; i++) {
-        removeCards[i].remove();
-    }
-
-    // Loop over library and display to book cards
-    let index = 0;
-    myLibrary.forEach(Library => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        books.appendChild(card);
-
-        // Create remove book button
-        const removeBook = document.createElement("button");
-        removeBook.classList.add(".remove-book-btn");
-        removeBook.textContent = "Remove Book";
-        removeBook.dataset.linkedArray = index;
-        index++;
-        card.appendChild(removeBook);
-
-        // Remove book from library when button is clicked
-        removeBook.addEventListener("click", removeBookFromLibrary);
-
-        function removeBookFromLibrary() {
-            let fetchBookToRemove = removeBook.dataset.linkedArray;
-            myLibrary.splice(parseInt(fetchBookToRemove), 1);
-            card.remove();
-            displayBooks();
-        }
-
-        for (let key in Library) {
-            const para = document.createElement("p");
-            para.textContent = (`${key}: ${Library[key]}`);
-            card.appendChild(para);
-        }
-    })
-}
